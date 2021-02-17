bddc3b357c2fdfc258c9b648ed3544f2ed9828b3
pushu0 <pushul@gmail.com>
2021-02-17 19:56:31 +0000

    Dummy endpoints to test mongodb

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,11 +5,22 @@ dotenv.config()
 import express from "express";
 const app = express();
 import connectDb from "./db/connection";
+import Url from "./db/models/Url.model"
 
 const PORT = 8080;
 app.get("/", (req, res) => {
     res.send("Hello from Node.js app \n");
 });
+app.get("/urls", async (req, res) => {
+    const users = await Url.find();
+    res.json(users);
+});
+app.get("/url-create", async (req, res) => {
+    const user = new Url({ url: "urlTest" });
+    await user.save().then(() => console.log("User created"));
+    res.send("User created \n");
+});
+
 app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`);
 });
