9534dfa5612023d33be6fb98aab007d8b1c553d5
pushu0 <pushul@gmail.com>
2021-02-17 19:38:58 +0000

    Remove eslint / add tslint / mongoose / express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,16 @@
-const dotenv = require('dotenv')
+
+import dotenv from 'dotenv';
 dotenv.config()
+
+import express from "express";
+const app = express();
+
+const PORT = 8080;
+app.get("/", (req, res) => {
+ res.send("Hello from Node.js app \n");
+});
+app.listen(PORT, function() {
+ console.log(`Listening on ${PORT}`);
+});
 
 console.log(`Your port is ${process.env.MONGO_DB}`)
