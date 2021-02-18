45e48514488a71caa99e2d0939eb9549228c0548
pushu0 <pushul@gmail.com>
2021-02-18 17:00:35 +0000

    Update the model with the short url virtual and validation

--- api/src/db/models/Url.model.ts
+++ api/src/db/models/Url.model.ts
@@ -1,14 +1,36 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
+
+const prefix = process.env.URL_PREFIX || 'pbid.io'
+
+interface IUrl extends Document{
+    url: string,
+    id: string,
+    short: string
+}
+
 const urlSchema = new Schema({
     url: {
         type: String,
         required: true
     },
-    short: {
-        type: String,  
-        required: true  
-        // default: nonoid
+    id: {
+        type: String,
+        required: true,
+        unique: true
     }
 });
-const Url = model("Url", urlSchema);
+
+urlSchema.virtual('short').get(function() {
+    const url = new URL(this.url)
+    return `${url.protocol}//${prefix}/${this.id}`
+});
+
+urlSchema.path('url').validate((val: string) => {
+    const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+    return urlRegex.test(val);
+}, 'Invalid URL.');
+
+const Url = model<IUrl>("Url", urlSchema);
 export default Url;
